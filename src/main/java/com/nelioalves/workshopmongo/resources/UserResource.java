4bbd2cd7b8c0af2c88b775d07a2441a5fe7cbf5e
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-10 12:05:31 -0300

    :sparkles: feat: Adds user service dependency injection in UserResource.

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.nelioalves.workshopmongo.resources;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +15,9 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+  @Autowired
+  private UserService service;
+
   @RequestMapping(method = RequestMethod.GET)
   public ResponseEntity<List<User>> findAll() {
     User maria = new User("1", "Maria Brown", "maria@gmail.com");
