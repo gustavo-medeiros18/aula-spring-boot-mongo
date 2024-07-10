0855bd4d2f503cfc6ada69babc5e00dcdeee4528
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-10 15:07:22 -0300

    :memo: docs: Adds findById() method in UserService.

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.nelioalves.workshopmongo.services;
 
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.UserRepository;
+import com.nelioalves.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -14,5 +16,13 @@ public class UserService {
 
   public List<User> findAll() {
     return repo.findAll();
+  }
+
+  public User findById(String id) {
+    try {
+      return repo.findById(id).orElseThrow();
+    } catch (NoSuchElementException e) {
+      throw new ObjectNotFoundException("Objeto não encontrado");
+    }
   }
 }
