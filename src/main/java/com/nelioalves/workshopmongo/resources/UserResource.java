6a7bda3774f1ed6dd1238bf4e7d943865382f03a
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-10 15:08:07 -0300

    :memo: docs: Adds findById() method in UserResource.

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.nelioalves.workshopmongo.dto.UserDto;
 import com.nelioalves.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,11 @@ public class UserResource {
         .collect(Collectors.toList());
 
     return ResponseEntity.ok().body(listDto);
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public ResponseEntity<UserDto> findById(@PathVariable String id) {
+    User obj = service.findById(id);
+    return ResponseEntity.ok().body(new UserDto(obj));
   }
 }
