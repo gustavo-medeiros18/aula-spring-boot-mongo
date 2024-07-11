da8dfbf7ff35b4de7cddd3c485386c7061e7e195
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-11 12:00:24 -0300

    :sparkles: feat: Inserts comments into posts while seeding.

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.nelioalves.workshopmongo.config;
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.dto.AuthorDTO;
+import com.nelioalves.workshopmongo.dto.CommentDTO;
 import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,6 +54,13 @@ public class Instantiation implements CommandLineRunner {
         "Acordei feliz hoje!",
         new AuthorDTO(maria)
     );
+
+    CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+    CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+    CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+    post1.getComments().addAll(Arrays.asList(c1, c2));
+    post2.getComments().addAll(Arrays.asList(c3));
 
     postRepository.saveAll(Arrays.asList(post1, post2));
 
