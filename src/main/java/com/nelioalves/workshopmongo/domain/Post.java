a49d75c2e7719d56668f854a0427aaa2f2af7d97
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-11 11:59:34 -0300

    :sparkles: feat: Adds comments field to Post entity class.

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.nelioalves.workshopmongo.domain;
 
 import com.nelioalves.workshopmongo.dto.AuthorDTO;
+import com.nelioalves.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -16,6 +19,8 @@ public class Post implements Serializable {
   private String title;
   private String body;
   private AuthorDTO author;
+
+  private List<CommentDTO> comments = new ArrayList<>();
 
   public Post() {
 
@@ -49,6 +54,10 @@ public class Post implements Serializable {
     return author;
   }
 
+  public List<CommentDTO> getComments() {
+    return comments;
+  }
+
   public void setId(String id) {
     this.id = id;
   }
@@ -67,6 +76,10 @@ public class Post implements Serializable {
 
   public void setAuthor(AuthorDTO author) {
     this.author = author;
+  }
+
+  public void setComments(List<CommentDTO> comments) {
+    this.comments = comments;
   }
 
   @Override
