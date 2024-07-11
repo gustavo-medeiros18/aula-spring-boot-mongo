305575f1f72593cc088a03dfeeb696f2424ba63e
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-11 11:13:05 -0300

    :sparkles: feat: Adds PostService and PostResource with findById() method.

---
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.nelioalves.workshopmongo.services;
+
+import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.repository.PostRepository;
+import com.nelioalves.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class PostService {
+  @Autowired
+  private PostRepository repo;
+
+  public Post findById(String id) {
+    try {
+      return repo.findById(id).orElseThrow();
+    } catch (NoSuchElementException e) {
+      throw new ObjectNotFoundException("Objeto não encontrado");
+    }
+  }
+}
