815d4ceca6177f3f67259177d0d04b73a90c30af
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-11 10:28:32 -0300

    :sparkles: feat: Links saved user (author) to post when seeding.

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.nelioalves.workshopmongo.config;
 
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.AuthorDTO;
 import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,28 +27,33 @@ public class Instantiation implements CommandLineRunner {
     sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
     userRepository.deleteAll();
+    postRepository.deleteAll();
 
     User maria = new User(null, "Maria Brown", "maria@gmail.com");
     User alex = new User(null, "Alex Green", "alex@gmail.com");
     User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+    /**
+     * Users need to be saved before posts because posts reference users.
+     * If not, the users nested in the posts will have a null id.
+     */
+    userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
     Post post1 = new Post(
         null,
         sdf.parse("21/03/2018"),
         "Partiu viagem",
         "Vou viajar para São Paulo. Abraços!",
-        maria
+        new AuthorDTO(maria)
     );
     Post post2 = new Post(
         null,
         sdf.parse("23/03/2018"),
         "Bom dia",
         "Acordei feliz hoje!",
-        maria
+        new AuthorDTO(maria)
     );
 
-
-    userRepository.saveAll(Arrays.asList(maria, alex, bob));
     postRepository.saveAll(Arrays.asList(post1, post2));
   }
 }
