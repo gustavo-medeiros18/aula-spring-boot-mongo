15b5e53a7800df978dfa6b32d904c6fa517634fb
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-12 09:13:03 -0300

    :sparkles: feat: Adds fullsearch() query method in PostRepository interface.

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
   /**
    * Spring data supports custom query methods. For creating a custom
    * query method, we need to follow a naming convention. Spring will
@@ -22,4 +23,15 @@ public interface PostRepository extends MongoRepository<Post, String>{
    */
   @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
   List<Post> searchTitle(String text);
+
+  @Query("{ $and: [ " +
+      "{ date: { $gte: ?1 } }, " +
+      "{ date: { $lte: ?2 } }, " +
+      "{ $or: [ " +
+      "{ 'title': { $regex: ?0, $options: 'i' } }, " +
+      "{ 'body': { $regex: ?0, $options: 'i' } }, " +
+      "{ 'comments.text': { $regex: ?0, $options: 'i' } } " +
+      "] } " +
+      "] }")
+  List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
